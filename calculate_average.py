b63c5c0259ce74293fc85e95f5d4530ae0d73f0a
flavorbreaker <turner.l.anthony@gmail.com>
2023-05-18 21:04:18 -0400

    Added descriptive comments to the solution

--- calculate_average.py
+++ calculate_average.py
@@ -1,8 +1,8 @@
-numbers = [1, 2, 3]
+numbers = [1, 2, 3]                 # creates a list of integers
 
 
-def find_average(nums):
-    nums_sum = 0
-    for x in nums:
-        nums_sum += x
-    return nums_sum / len(nums)
+def find_average(nums):             # creates the function find_average and passes the nums parameter
+    nums_sum = 0                    # creates a variable to hold the sum of nums, initializing to 0
+    for x in nums:                  # loops over every item in nums
+        nums_sum += x               # adds each item in nums to nums_sum
+    return nums_sum / len(nums)     # returns nums_sum divided by the number of indices in nums
